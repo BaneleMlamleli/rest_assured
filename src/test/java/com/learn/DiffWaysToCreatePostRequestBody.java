39e8703c97845c8260f36c164b7bcdf4a136310d
BaneleMlamleli <mlamlelibanele@yahoo.com>
2024-04-06 03:51:59 +0200

    Implimented different ways of creating POST request payload. I also installed json-server to mock REST API locally

---
+++ src/test/java/com/learn/DiffWaysToCreatePostRequestBody.java
@@ -0,0 +1,102 @@
+package com.learn;
+
+import org.json.JSONTokener;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import io.restassured.http.ContentType;
+
+public class DiffWaysToCreatePostRequestBody {
+
+    //Post request body using a HashMap
+    //@Test
+    public void postUsingHashMap() {
+        System.out.println("Executed method: 'postUsingHashMap()'");
+        baseURI = "http://localhost:3000";
+        HashMap<String, Object> mapData = new HashMap<String, Object>();
+        mapData.put("email", "newstudent@student.com");
+        mapData.put("first_name", "usedHashMap");
+        mapData.put("last_name", "usedHashMap");
+        mapData.put("avatar", "https://reqres.in/img/faces/8-image.jpg");
+        String arrayCourse[] = { "Engineering and Design I", "Electrical Engineering II" };
+        mapData.put("courses", arrayCourse);
+
+        given()
+            .contentType(ContentType.JSON).accept(ContentType.JSON)
+            .body(mapData)
+        .when()
+            .post("/student")
+        .then()
+            .statusCode(201);
+    }
+    
+    //Post request body using org.json
+    //@Test
+    public void postUsingOrgJsonDependency(){
+        System.out.println("Executed method: 'postUsingOrgJsonDependency()'");
+        baseURI = "http://localhost:3000";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("email", "newemployee@employee.com");
+        jsonObject.put("first_name", "usedJSONObject");
+        jsonObject.put("last_name", "usedJSONObject");
+        jsonObject.put("avatar", "https://reqres.in/img/faces/8-image.jpg");
+
+        given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(jsonObject.toJSONString())
+        .when()
+            .post("/employees")
+        .then()
+            .statusCode(201);
+    }
+    
+    //Post request body using POJO class
+    //@Test
+    public void postUsingPojoClass() {
+        System.out.println("Executed method: 'postUsingPojoClass()'");
+        baseURI = "http://localhost:3000";
+        Data data = new Data("newemployee@employee.com", "usedPojo", "usedPojo", "https://reqres.in/img/faces/11-image.jpg");
+        
+        given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(data)
+        .when()
+            .post("/employees")
+        .then()
+            .statusCode(201).log().all();
+    }
+    
+    //Post request body using external json file data
+    @Test
+    public void postUsingExternalJsonFile(){
+        System.out.println("Executed method: 'postUsingExternalJsonFile'");
+        baseURI = "http://localhost:3000";
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File("/dataFile.json"));
+            JSONTokener jsonTokener = new JSONTokener(fileInputStream); 
+            org.json.JSONObject jsonObject = new org.json.JSONObject(jsonTokener);
+            jsonObject.put("email", "newemployee@employee.com");
+            jsonObject.put("first_name", "usedJSONObject");
+            jsonObject.put("last_name", "usedJSONObject");
+            jsonObject.put("avatar", "https://reqres.in/img/faces/8-image.jpg");
+            
+            given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(jsonObject.toString())
+            .when()
+                .post("/employees")
+            .then()
+                .statusCode(201).log().all();
+        } catch (FileNotFoundException e) {
+        }
+    }
+}
---
+++ src/test/java/com/learn/ManageUsers.java
@@ -0,0 +1,77 @@
+package com.learn;
+
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class ManageUsers {
+
+    @DataProvider(name = "createUser")
+    public Object[][] getData() {
+        Object[][] data = { { "Banele", "QA Engineer" }, { "Sethu", "Dental Assistant" }, { "Sam", "baby" } };
+        return data;
+    }
+
+    @Test
+    public void getAllUsers() {
+        Response restResponse = get("https://reqres.in/api/users?page=2");
+        System.out.println("Time: " + restResponse.getTime());
+        System.out.println("Status code: " + restResponse.statusCode());
+        System.out.println("Body: " + restResponse.getBody().asString());
+
+        Assert.assertEquals(restResponse.statusCode(), 200);
+    }
+
+    @Test
+    public void getSingleUser() {
+        baseURI = "https://reqres.in/api";
+        given().get("/users/2").then().statusCode(200);
+    }
+
+    @Test(dataProvider = "createUser")
+    public void postNewUser(String name, String jobTitle) {
+        baseURI = "https://reqres.in/api";
+        JSONObject jsonObjectMap = new JSONObject();
+        jsonObjectMap.put("name", name);
+        jsonObjectMap.put("job", jobTitle);
+        System.out.println(jsonObjectMap);
+
+        given().contentType(ContentType.JSON).accept(ContentType.JSON).body(jsonObjectMap.toJSONString()).when().post("/users").then().statusCode(201).log().all();
+    }
+    
+    @Test
+    public void putUserUpdate() {
+        baseURI = "https://reqres.in/api";
+        Map<String, Object> userDetailsMap = new HashMap<String, Object>();
+        userDetailsMap.put("name", "Banele01");
+        userDetailsMap.put("job", "QA Engineer01");
+        JSONObject jsonObjectMap = new JSONObject(userDetailsMap);
+
+        given().contentType(ContentType.JSON).accept(ContentType.JSON).body(jsonObjectMap.toJSONString()).when().put("/users/2").then().statusCode(200).log().all();
+    }
+    
+    @Test
+    public void patchUserUpdate() {
+        baseURI = "https://reqres.in/api";
+        JSONObject jsonObjectMap = new JSONObject();
+        jsonObjectMap.put("name", "Banele02");
+        jsonObjectMap.put("job", "QA Engineer02");
+        System.out.println(jsonObjectMap);
+
+        given().contentType(ContentType.JSON).accept(ContentType.JSON).body(jsonObjectMap.toJSONString()).when().patch("/users/2").then().statusCode(200).log().all();
+    }
+    
+    @Test
+    public void deleteUser() {
+        baseURI = "https://reqres.in/api";
+        when().delete("/users/2").then().statusCode(204).log().all();
+    }
+}
